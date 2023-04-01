5baadfad1530c286c0cb4c08e6de49f5cabbdcc9
kokaresamarth <kokaresamarth0@gmail.com>
2023-04-01 21:51:01 +0530

    string

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+package reversestring;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Samarth";
+		int leng=name.length();
+		String rev="";
+		for(int i=leng-1; i>=0; i++)
+		{
+			rev=rev+name.charAt(i);
+		}
+		System.out.println("Reverse of "+name+"is"+rev);
+	}
+
+}
